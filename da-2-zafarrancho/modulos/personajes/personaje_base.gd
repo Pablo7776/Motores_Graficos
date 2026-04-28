7d54b664f74d1414e24ebaeac36cf61fb360f8cd
Jipilucas <zvitsova@gmail.com>
2026-04-28 00:03:37 -0300

    Pequeño cambio en el cambio de turno
    Ahora queda el estado Idle activo aunque cuando pasaste el turno estuvieras corriendo.
    
    Faltaría ver como hacer para que se caiga del aire si no es su turno.

--- da-2-zafarrancho/modulos/personajes/personaje_base.gd
+++ da-2-zafarrancho/modulos/personajes/personaje_base.gd
@@ -38,6 +38,7 @@ func _physics_process(delta):
 
 	state_machine.update(delta)
 	#Esto hace que se termine el turno
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel") and is_on_floor():
+			state_machine.change_state(state_machine.idle_state)
 			$"../GestorDeTurnos".siguiente_turno()
 	move_and_slide()
