00e312f01b5b656a54351bbc5da745dbe67a8cd1
Pablo Duran <86197227+Pablo7776@users.noreply.github.com>
2026-06-23 19:51:33 -0300

    4 personajes

--- da-2-zafarrancho/Escenas/player_card.gd
+++ da-2-zafarrancho/Escenas/player_card.gd
@@ -2,10 +2,14 @@ extends PanelContainer
 
 @onready var selector1: Selector = $VBoxContainer/HBoxContainer
 @onready var selector2: Selector = $VBoxContainer/HBoxContainer2
+@onready var selector3: Selector = $VBoxContainer/HBoxContainer3
+@onready var selector4: Selector = $VBoxContainer/HBoxContainer4
 
 func get_data() -> Dictionary:
 	return {
 		"personaje1": selector1.get_personaje(),
-		"personaje2": selector2.get_personaje()
+		"personaje2": selector2.get_personaje(),
+		"personaje3": selector3.get_personaje(),
+		"personaje4": selector4.get_personaje()
 	}
 	
