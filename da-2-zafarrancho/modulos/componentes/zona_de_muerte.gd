b425406ab500ffca80308f10d7f07302919b53ea
Jipilucas <zvitsova@gmail.com>
2026-06-23 19:46:20 -0300

    fix_pasodeturno_por_caida
    Ahora solo pasa el turno si el pj que cayó era el del turno activo.

--- da-2-zafarrancho/modulos/componentes/zona_de_muerte.gd
+++ da-2-zafarrancho/modulos/componentes/zona_de_muerte.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is PersonajeBase:
-		$"../GestorDeTurnosV2".siguiente_turno()
+		if body.es_mi_turno:
+			$"../GestorDeTurnosV2".siguiente_turno()
 		var gestor_de_vida = body.get_node("HealthManager")
 		var vida_actual = gestor_de_vida.get_health()
 		body.get_node("Hurtbox").take_damage(vida_actual)
