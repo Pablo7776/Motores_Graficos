67b01b91d95557f00124d58edef42238ed270a07
Jipilucas <zvitsova@gmail.com>
2026-06-23 14:26:23 -0300

    fix_zona_de_muerte
    Se configuró el paso de turno cuando el pj muere por caida. Pero hay que ver que pasa si caen PJ que no son los que están en turno actualmente.

--- da-2-zafarrancho/modulos/componentes/zona_de_muerte.gd
+++ da-2-zafarrancho/modulos/componentes/zona_de_muerte.gd
@@ -1,4 +1,9 @@
 extends Area2D
 
+
 func _on_body_entered(body):
-	body.get_node("Hurtbox").take_damage(3)
+	if body is PersonajeBase:
+		$"../GestorDeTurnosV2".siguiente_turno()
+		var gestor_de_vida = body.get_node("HealthManager")
+		var vida_actual = gestor_de_vida.get_health()
+		body.get_node("Hurtbox").take_damage(vida_actual)
