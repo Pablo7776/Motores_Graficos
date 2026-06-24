2b52400cecb289c6fda9e295cec58f513ac0ed13
Rocio-ux <rociolujansantos@gmail.com>
2026-06-24 11:21:12 -0300

    revancha
    conecte el botón revancha a la escena del juego

--- da-2-zafarrancho/victoria.gd
+++ da-2-zafarrancho/victoria.gd
@@ -8,3 +8,7 @@ func _ready() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/menu_principal.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://juego/main.tscn")
